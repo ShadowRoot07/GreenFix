a8c80d323630e4b8a6534c7bd8a57893515f8dd2
MaycolAp <maycol18ap@gmail.com>
2026-05-24 22:22:50 -0400

    update de fondo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,22 @@ import Dashboard from "./views/Dashboard";
 import ProjectDetail from "./views/ProjectDetail";
 import CreateProject from "./views/CreateProject";
 import { useApp } from "./context/AppContext";
+import logoFondo from './assets/logo.svg'
 
 function App() {
   const { activeView } = useApp();
 
   return (
-    <div className="min-h-screen bg-background text-text-primary">
+    <div className="relative min-h-screen bg-background text-text-primary">
+      
+      {/* CAPA DEL LOGO: Subimos a z-0 y subimos la opacidad para estar 100% seguros de verlo */}
+      <div className="fixed inset-0 z-0 flex items-center justify-center pointer-events-none opacity-20">
+        <img 
+          src={logoFondo} 
+          alt="GreenFix Background Logo" 
+          className="w-[50%] max-w-2xl object-contain select-none"
+        />
+      </div>
       <Navbar />
       <AuthModal />
 
